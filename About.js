9fa78c7825fa6f91e0833fd7e2f7c4afd786dadb
puneet3036 <puneetsharma3036@gmail.com>
2022-06-22 06:22:42 +0530

    new

---
+++ About.js
@@ -0,0 +1,12 @@
+   
+   $(document).ready(function()
+   {
+      $('#ThemeableMenu1 .dropdown-toggle').dropdown({popperConfig:{placement:'bottom-start',modifiers:{computeStyle:{gpuAcceleration:false}}}});
+      $(document).on('click','.ThemeableMenu1-navbar-collapse.show',function(e)
+      {
+         if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) 
+         {
+            $(this).collapse('hide');
+         }
+      });
+   });
